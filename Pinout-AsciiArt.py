492c50872c5b501905e7d535258fdf93c307d060
fbez <francesco.bez@protonmail.com>
2023-11-17 16:31:33 +0100

    First working draft

--- Pinout-AsciiArt.py
+++ Pinout-AsciiArt.py
@@ -1,15 +1,16 @@
 from kiutils.symbol import SymbolLib, Symbol, SymbolPin
 
 def print_footprint(PN_dict):
-    pin_nr = int(max([int(k) for k in PN_dict.keys()])) # get the nr of pins
+    pin_nr = int(max([int(k) for k in PN_dict["pins"].keys()])) # get the nr of pins
     return_string = []
-    left_pins_max_length = max([len(PN_dict[str(k+1)]) for k in range(pin_nr//2)])
+    left_pins_max_length = max([len(PN_dict["pins"][str(k+1)]) for k in range(pin_nr//2)])
     right_pin_nr_length = len(str(pin_nr))
     for k in range(pin_nr//2):
-        return_string.append(PN_dict[str(k+1)].upper().ljust(left_pins_max_length)+'|['+str(k+1)+']\t'+('['+str(pin_nr-k)).rjust(right_pin_nr_length+1)+']|'+PN_dict[str(pin_nr-k)].upper())
+        return_string.append(PN_dict["pins"][str(k+1)].upper().ljust(left_pins_max_length)+'|['+str(k+1)+']\t'+('['+str(pin_nr-k)).rjust(right_pin_nr_length+1)+']|'+PN_dict["pins"][str(pin_nr-k)].upper())
 
     return_string.insert(0,(' '*(left_pins_max_length))+'+'+('-'*(6+right_pin_nr_length))+'+')
     return_string.append((' '*(left_pins_max_length))+"+"+('-'*(6+right_pin_nr_length))+"+")
+    
     for k in return_string:
         print(k)
 
@@ -30,17 +31,31 @@ for k in symbolLib.symbols:
         print(j.entryName)
         if(j.entryName not in ret_dict.keys()):
             ret_dict[j.entryName]={}
+            ret_dict[j.entryName]["pins"]={}
+            ret_dict[j.entryName]["name"]=k.entryName
+            ret_dict[j.entryName]["description"]=""
+            ret_dict[j.entryName]["datasheet"]=""
+            for l in k.properties:
+                if(l.key == "Datasheet"):
+                    ret_dict[j.entryName]["datasheet"]=l.value
+                if(l.key == "ki_description"):
+                    ret_dict[j.entryName]["description"]=l.value
+
         for i in j.pins:
-            print(i.name)
-            print(i.number)
-            ret_dict[j.entryName][i.number]=i.name
+            ret_dict[j.entryName]["pins"][i.number]=i.name
 
 
 
+PN = '74HC595'
 
-print(ret_dict['74HC595'])
+footprint = (print_footprint(ret_dict[PN]))
+temp = PN+'\n' + ('='*len(PN))+'\n\n'
+temp += "Description: "+ret_dict[PN]["description"]+'\n'
+temp += "Datasheet: "+ret_dict[PN]["datasheet"]+'\n\n'
+temp += "Pinout: "+'\n'+("-"*8)+'\n\n'+footprint
 
-temp = (print_footprint(ret_dict['74LS298']))
+
+
 f = open('text.txt','w')
 f.write(temp)
 f.close()
