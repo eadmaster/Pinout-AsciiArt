e87f437ab8672f6c45ac57e3f56315e8206e90fd
fbez <francesco.bez@protonmail.com>
2023-11-17 16:47:12 +0100

    test temp.md

--- Pinout-AsciiArt.py
+++ Pinout-AsciiArt.py
@@ -50,9 +50,9 @@ PN = '74HC595'
 
 footprint = (print_footprint(ret_dict[PN]))
 temp = '#'+PN+'\n' + ('='*len(PN))+'\n\n'
-temp += "*Description*: "+ret_dict[PN]["description"]+'\n'
+temp += "*Description*: "+ret_dict[PN]["description"]+'\n\n'
 temp += "*Datasheet*: "+ret_dict[PN]["datasheet"]+'\n\n'
-temp += "*Pinout*: "+'\n'+("-"*8)+'\n\n```'+footprint+'\n```'
+temp += "*Pinout*: "+'\n'+'\n\n```'+footprint+'\n```'
 
 
 
